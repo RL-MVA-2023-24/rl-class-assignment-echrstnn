228433e38dc9278eb42a555877d8f133f1ff1bca
echrstnn <estebanc@protonmail.ch>
2024-03-04 18:01:07 +0000

    truc

--- src/train.py
+++ src/train.py
@@ -1,5 +1,9 @@
 from gymnasium.wrappers import TimeLimit
 from env_hiv import HIVPatient
+import numpy as np
+import torch
+from torch import nn
+from torch.nn import functional as F
 
 env = TimeLimit(
     env=HIVPatient(domain_randomization=False), max_episode_steps=200
@@ -10,13 +14,33 @@ env = TimeLimit(
 # You have to implement your own agent.
 # Don't modify the methods names and signatures, but you can add methods.
 # ENJOY!
-class ProjectAgent:
+class ProjectAgent(nn.Module):
+    def __init__(self) -> None:
+        super().__init__()
+        self.layers = nn.Sequential(
+            nn.Linear(6, 256),
+            nn.LeakyReLU(),
+            nn.Linear(256, 256),
+            nn.LeakyReLU(),
+            nn.Linear(256, 4),
+        )
+
     def act(self, observation, use_random=False):
-        # print(f"\nact\nobs={observation}\nrandom={use_random}")
-        return 0
+        with torch.no_grad():
+            observation = torch.tensor(observation, dtype=torch.float32).unsqueeze(0)
+            act_val = self(observation).squeeze()
+            act = act_val.argmax().item()
+            print(f"{act}  {act_val.numpy()}")
+            return act
+
+    def forward(self, observation):
+        x = torch.log10(observation)
+        x = self.layers(x)
+        return torch.exp(x)
 
     def save(self, path):
-        pass
+        torch.save(self.state_dict(), path)
 
     def load(self):
-        pass
+        print("Loading...")
+        self.load_state_dict(torch.load("model.pt"))
